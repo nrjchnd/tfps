5883f74f785e33111fb8810e1270a9a75a6028d5
root <root@ip-172-31-53-39.ec2.internal>
2021-08-29 11:35:47 +0000

    Clean database

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-m4 defines.m4 tfps.m4 >/etc/opensips/opensips.cfg
-m4 defines.m4 sip.conf.m4 >/etc/asterisk/sip.conf
-m4 defines.m4 extensions.ael.m4 >/etc/asterisk/extensions.ael
-m4 defines.m4 msmtprc.m4 >/etc/msmtprc
-cp  extensions.conf /etc/asterisk/extensions.conf
-cp  modules.conf /etc/asterisk/modules.conf
-cp  asterisk.conf /etc/asterisk/asterisk.conf
-cp tfpsctl /usr/bin
-systemctl restart opensips
-systemctl restart asterisk
+sudo git pull
+sudo m4 defines.m4 tfps.m4 >/etc/opensips/opensips.cfg
+sudo m4 defines.m4 sip.conf.m4 >/etc/asterisk/sip.conf
+sudo m4 defines.m4 extensions.ael.m4 >/etc/asterisk/extensions.ael
+sudo m4 defines.m4 msmtprc.m4 >/etc/msmtprc
+sudo cp  extensions.conf /etc/asterisk/extensions.conf
+sudo cp  modules.conf /etc/asterisk/modules.conf
+sudo cp  asterisk.conf /etc/asterisk/asterisk.conf
+sudo cp  tfpsctl /usr/bin
+sudo systemctl restart opensips
+sudo systemctl restart asterisk
 
